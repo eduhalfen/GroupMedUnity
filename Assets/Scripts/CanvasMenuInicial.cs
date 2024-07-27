c820c7321082c0d23b70dd38be0daf63c22eae40
Eduardo Halfen <eduhalfen@gmail.com>
2024-07-27 00:16:47 -0300

    Telas LED Capacitacao e Suporte

--- Assets/Scripts/CanvasMenuInicial.cs
+++ Assets/Scripts/CanvasMenuInicial.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
-
+using Febucci.UI;
 
 
 
@@ -26,7 +26,10 @@ public class CanvasMenuInicial : MonoBehaviour
     bool isScreenSaver = false;
 
     public Animator animatorTelas, animatorLED;
-    public AudioSource audioSourceTransition;
+    public AudioSource audioSource;
+    public AudioClip audioEntraTela, audioSaiTela, audioSegundaTela;
+
+    public TextAnimatorPlayer textAnimatorPlayerPessoas, textAnimatorPlayerTecnologia, textAnimatorPlayerProcessos;
 
 
 
@@ -63,7 +66,8 @@ public class CanvasMenuInicial : MonoBehaviour
 
         }
 
-        PlayerPrefs.SetInt("Bloco", 0);
+        AudioSaitela();
+        StartCoroutine(EntraTextoTelaPrincipal());
     }
 
 
@@ -94,81 +98,168 @@ public class CanvasMenuInicial : MonoBehaviour
         */
     }
 
+
+    IEnumerator EntraTextoTelaPrincipal()
+    {
+        textAnimatorPlayerPessoas.ShowText("");
+        textAnimatorPlayerTecnologia.ShowText("");
+        textAnimatorPlayerProcessos.ShowText("");
+        yield return new WaitForSeconds(0.75f);
+
+        textAnimatorPlayerPessoas.ShowText("Pessoas");
+        textAnimatorPlayerPessoas.StartShowingText(true);
+
+        textAnimatorPlayerTecnologia.ShowText("Tecnologia");
+        textAnimatorPlayerTecnologia.StartShowingText(true);
+
+        textAnimatorPlayerProcessos.ShowText("Processos");
+        textAnimatorPlayerProcessos.StartShowingText(true);
+    }
+
+    void SaiTextoTelaPrincipal()
+    {
+        /*
+        //textAnimatorPlayerPessoas.StartDisappearingText();
+        textAnimatorPlayerPessoas.ShowText("        ");
+        textAnimatorPlayerPessoas.StartShowingText(false);
+        */
+    }
+
+
+    void AudioEntraTela()
+    {
+        audioSource.clip = audioEntraTela;
+        audioSource.Play();
+    }
+
+    void AudioSaitela()
+    {
+        audioSource.clip = audioSaiTela;
+        audioSource.Play();
+    }
+
+    void AudioSegundaTela()
+    {
+        audioSource.clip = audioSegundaTela;
+        audioSource.Play();
+    }
+
+
     //--------------------------------------------------------------
     public void PressionaButtonCapacitacao()
     {
         animatorTelas.SetBool("EntraCapacitacao", true);
-        audioSourceTransition.Play();
+        animatorLED.SetBool("EntraCapacitacao", true);
+        AudioEntraTela();
     }
 
     public void PressionaButtonVoltarCapacitacao()
     {
         animatorTelas.SetBool("EntraCapacitacao", false);
-        audioSourceTransition.Play();
+        animatorLED.SetBool("EntraCapacitacao", false);
+        animatorLED.SetBool("EntraGPTW", false);
+        animatorLED.SetBool("EntraFormacao", false);
+        AudioSaitela();
+        StartCoroutine(EntraTextoTelaPrincipal());
     }
 
+    public void PressionaGPTW()
+    {
+        animatorLED.SetBool("EntraGPTW", true);
+        animatorLED.SetBool("EntraFormacao", false);
+        AudioSegundaTela();
+    }
+
+    public void PressionaFormacao()
+    {
+        animatorLED.SetBool("EntraFormacao", true);
+        animatorLED.SetBool("EntraGPTW", false);
+        AudioSegundaTela();
+    }
+
+
+    //--------------------------------------------------------------
     public void PressionaButtonSuporte()
     {
         animatorTelas.SetBool("EntraSuporte", true);
-        audioSourceTransition.Play();
+        animatorLED.SetBool("EntraSuporte", true);
+        AudioEntraTela();
     }
 
     public void PressionaButtonVoltarSuporte()
     {
         animatorTelas.SetBool("EntraSuporte", false);
-        audioSourceTransition.Play();
+        animatorLED.SetBool("EntraSuporte", false);
+        animatorLED.SetBool("EntraSuporteInterno", false);
+        AudioSaitela();
+        StartCoroutine(EntraTextoTelaPrincipal());
     }
 
+    public void PressionaSuporteInterno()
+    {
+        animatorLED.SetBool("EntraSuporteInterno", true);
+        AudioSegundaTela();
+    }
+
+    //--------------------------------------------------------------
     public void PressionaButtonPrevencao()
     {
         animatorTelas.SetBool("EntraPrevencao", true);
-        audioSourceTransition.Play();
+        animatorLED.SetBool("EntraPrevencao", true);
+        AudioEntraTela();
     }
 
     public void PressionaButtonVoltarPrevencao()
     {
         animatorTelas.SetBool("EntraPrevencao", false);
-        audioSourceTransition.Play();
+        animatorLED.SetBool("EntraPrevencao", false);
+        AudioSaitela();
+        StartCoroutine(EntraTextoTelaPrincipal());
     }
 
     public void PressionaButtonTecnologia()
     {
         animatorTelas.SetBool("EntraTecnologia", true);
-        audioSourceTransition.Play();
+        animatorLED.SetBool("EntraTecnologia", true);
+        AudioEntraTela();
     }
 
     public void PressionaButtonVoltarTecnologia()
     {
         animatorTelas.SetBool("EntraTecnologia", false);
-        audioSourceTransition.Play();
+        animatorLED.SetBool("EntraTecnologia", false);
+        AudioSaitela();
+        StartCoroutine( EntraTextoTelaPrincipal());
     }
 
     public void PressionaButtonLideres()
     {
         animatorTelas.SetBool("EntraLideres", true);
         animatorLED.SetBool("EntraLideres", true);
-        audioSourceTransition.Play();
+        AudioEntraTela();
     }
 
     public void PressionaButtonVoltarLideres()
     {
         animatorTelas.SetBool("EntraLideres", false);
         animatorLED.SetBool("EntraLideres", false);
-        audioSourceTransition.Play();
+        AudioSaitela();
+        StartCoroutine(EntraTextoTelaPrincipal());
     }
 
     public void PressionaButtonESG()
     {
         animatorTelas.SetBool("EntraESG", true);
         animatorLED.SetBool("EntraESG", true);
-        audioSourceTransition.Play();
+        AudioEntraTela();
     }
 
     public void PressionaButtonVoltarESG()
     {
         animatorTelas.SetBool("EntraESG", false);
         animatorLED.SetBool("EntraESG", false);
-        audioSourceTransition.Play();
+        AudioSaitela();
+        StartCoroutine(EntraTextoTelaPrincipal());
     }
 
     //-----------------------------------------------------------------
